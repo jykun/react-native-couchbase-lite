668b027577fe7720e87f369bc1813310c324b864
iwater <iwater@gmail.com>
2016-03-03 18:01:29 +0800

    add support for monitor db changes and copy database from prebundle

--- android/src/main/java/me/fraserxu/rncouchbaselite/ReactCBLite.java
+++ android/src/main/java/me/fraserxu/rncouchbaselite/ReactCBLite.java
@@ -1,7 +1,10 @@
 package me.fraserxu.rncouchbaselite;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 
+import com.couchbase.lite.Database;
+import com.couchbase.lite.DocumentChange;
 import com.couchbase.lite.Manager;
 import com.couchbase.lite.View;
 import com.couchbase.lite.android.AndroidContext;
@@ -9,12 +12,23 @@ import com.couchbase.lite.javascript.JavaScriptViewCompiler;
 import com.couchbase.lite.listener.Credentials;
 import com.couchbase.lite.listener.LiteListener;
 import com.couchbase.lite.util.Log;
+import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.android.*;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.modules.core.JavascriptException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ReactCBLite extends ReactContextBaseJavaModule {
 
@@ -24,6 +38,9 @@ public class ReactCBLite extends ReactContextBaseJavaModule {
     private ReactApplicationContext context;
     private int listenPort;
     private Credentials allowedCredentials;
+    private Manager managerServer;
+
+    private static final String DB_EVENT_KEY = "couchBaseDBEvent";
 
     public ReactCBLite(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -35,6 +52,16 @@ public class ReactCBLite extends ReactContextBaseJavaModule {
         return REACT_CLASS;
     }
 
+    /**
+     * Returns constants of this module in React-native to share (javascript)
+     */
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("DBChanged", DB_EVENT_KEY);
+        return constants;
+    }
+
     @ReactMethod
     public void init(int listenPort, String login, String password, Callback errorCallback) {
         initCBLite(listenPort, login, password, errorCallback);
@@ -42,31 +69,35 @@ public class ReactCBLite extends ReactContextBaseJavaModule {
 
     private void initCBLite(int listenPort, String login, String password, Callback errorCallback) {
         try {
-            allowedCredentials = new Credentials(login, password);
+            if(this.managerServer == null) {
+                allowedCredentials = new Credentials(login, password);
 
-            View.setCompiler(new JavaScriptViewCompiler());
+                View.setCompiler(new JavaScriptViewCompiler());
 
-            AndroidContext context = new AndroidContext(this.context);
-            Manager.enableLogging(Log.TAG, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_SYNC, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_QUERY, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_VIEW, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_CHANGE_TRACKER, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_BLOB_STORE, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_DATABASE, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_LISTENER, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_MULTI_STREAM_WRITER, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_REMOTE_REQUEST, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_ROUTER, Log.VERBOSE);
-            Manager manager = new Manager(context, Manager.DEFAULT_OPTIONS);
+                AndroidContext context = new AndroidContext(this.context);
+                Manager.enableLogging(Log.TAG, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_SYNC, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_QUERY, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_VIEW, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_CHANGE_TRACKER, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_BLOB_STORE, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_DATABASE, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_LISTENER, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_MULTI_STREAM_WRITER, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_REMOTE_REQUEST, Log.VERBOSE);
+                Manager.enableLogging(Log.TAG_ROUTER, Log.VERBOSE);
+                Manager manager = new Manager(context, Manager.DEFAULT_OPTIONS);
 
-            listenPort = startCBLListener(listenPort, manager, allowedCredentials);
+                listenPort = startCBLListener(listenPort, manager, allowedCredentials);
 
-            Log.i(TAG, "initCBLite() completed successfully with: " + String.format(
-                    "http://%s:%s@localhost:%d/",
-                    allowedCredentials.getLogin(),
-                    allowedCredentials.getPassword(),
-                    listenPort));
+                this.managerServer = manager;
+
+                Log.i(TAG, "initCBLite() completed successfully with: " + String.format(
+                        "http://%s:%s@localhost:%d/",
+                        allowedCredentials.getLogin(),
+                        allowedCredentials.getPassword(),
+                        listenPort));
+            }
           errorCallback.invoke();
 
         } catch (final Exception e) {
@@ -80,6 +111,87 @@ public class ReactCBLite extends ReactContextBaseJavaModule {
         int boundPort = listener.getListenPort();
         Thread thread = new Thread(listener);
         thread.start();
+
         return boundPort;
+    }
+
+    /**
+     * Function to be shared to React-native, it monitor a local database changes
+     * @param  databaseLocal    String      database for local server
+     * @param  onEnd            Callback    function to call when finish
+     */
+    @ReactMethod
+    public void monitorDatabase(String databaseLocal, Callback onEnd) {
+        try {
+            Manager manager = this.managerServer;
+            Database db = manager.getExistingDatabase(databaseLocal);
+
+            if(db != null) {
+                db.addChangeListener(new Database.ChangeListener() {
+                    @Override
+                    public void changed(Database.ChangeEvent event) {
+                        for (DocumentChange dc : event.getChanges()) {
+                            WritableMap eventM = Arguments.createMap();
+                            eventM.putString("databaseName", event.getSource().getName());
+                            eventM.putString("id", dc.getDocumentId());
+                            sendEvent(DB_EVENT_KEY, eventM);
+                        }
+                    }
+                });
+            }
+
+            if (onEnd != null)
+                onEnd.invoke();
+        }catch(Exception e){
+            throw new JavascriptException(e.getMessage());
+        }
+    }
+  
+    /**
+     * Function to be shared to React-native, copy a prebuild database if not exist
+     * @param  databaseLocal           String      database for local server
+     * @param  withPreBuildDatabase    String      database for preBuild
+     * @param  onEnd                   Callback    function to call when finish
+     */
+    @ReactMethod
+    public void copyDatabase(String databaseLocal, String withPreBuildDatabase, Callback onEnd) {
+        // create a manager
+        Manager manager;
+        try {
+            manager = new Manager(new AndroidContext(getReactApplicationContext()), Manager.DEFAULT_OPTIONS);
+        } catch (IOException e) {
+            Log.e(TAG, "Cannot create manager object");
+            return;
+        }
+
+        // setup database (copying initial data if needed)
+        Database _database;
+        try {
+            _database = manager.getExistingDatabase(databaseLocal);
+            if (_database == null) {
+                Log.i(TAG, "Database not found, extracting initial dataset.");
+                try {
+                    AssetManager assets = getReactApplicationContext().getAssets();
+                    InputStream cannedDb = assets.open("couchtalk.cblite");
+                    manager.replaceDatabase(databaseLocal, cannedDb, null);
+                } catch (IOException e) {
+                    Log.e(TAG, String.format("Couldn't load canned database. %s", e));
+                }
+                // HACK: intentionally may remain `null` so app crashes instead of silent trouble…
+                _database = manager.getExistingDatabase(databaseLocal);
+            }
+        } catch (CouchbaseLiteException e) {
+            Log.e(TAG, "Cannot get database");
+            return;
+        }
+        final Database database = _database;
+    }
+
+    /**
+     * Function to send: push pull events
+     */
+    private void sendEvent(String eventName, WritableMap params) {
+        getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
     }
 }
--- ios/ReactCBLite.m
+++ ios/ReactCBLite.m
@@ -16,12 +16,21 @@
 
 @implementation ReactCBLite
 
+NSString* const DB_CHANGED = @"couchBaseDBEvent";
+
 RCT_EXPORT_MODULE()
+
+- (id)init
+{
+    self = [super init];
+    return self;
+}
 
 RCT_EXPORT_METHOD(init:(float)port username:(NSString *)username password:(NSString *)password callback:(RCTResponseSenderBlock)callback)
 {
     NSLog(@"Launching Couchbase Lite...");
     CBLManager* dbmgr = [CBLManager sharedInstance];
+    [CBLManager enableLogging: @"Sync"];
     CBLRegisterJSViewCompiler();
     
     CBLListener* listener = [[CBLListener alloc] initWithManager:dbmgr port:port];
@@ -30,6 +39,56 @@ RCT_EXPORT_METHOD(init:(float)port username:(NSString *)username password:(NSStr
     
     NSLog(@"Couchbase Lite url = %@", listener.URL);
     callback(@[[NSNull null]]);
+}
+
+RCT_EXPORT_METHOD(monitorDatabase:(NSString *)databaseLocal callback:(RCTResponseSenderBlock)callback)
+{
+    NSError* error;
+    CBLManager* dbmgr = [CBLManager sharedInstance];
+    CBLDatabase* database = [dbmgr databaseNamed:databaseLocal error:&error];
+
+    [[NSNotificationCenter defaultCenter] addObserverForName: kCBLDatabaseChangeNotification
+                                                      object: database
+                                                       queue: nil
+                                                  usingBlock: ^(NSNotification *n) {
+                                                      NSArray* changes = n.userInfo[@"changes"];
+                                                      for (CBLDatabaseChange* change in changes) {
+                                                          NSDictionary* map = @{
+                                                                                @"databaseName": database.name,
+                                                                                @"id": change.documentID
+                                                                                };
+                                                          [self.bridge.eventDispatcher sendAppEventWithName:DB_CHANGED body:map];
+                                                          NSLog(@"Document '%@' changed.", change.documentID);
+                                                      }
+                                                  }
+     ];
+     callback(@[[NSNull null]]);
+}
+
+RCT_EXPORT_METHOD(copyDatabase:(NSString *)databaseLocal withPreBuildDatabase:(NSString *)withPreBuildDatabase callback:(RCTResponseSenderBlock)callback)
+{
+    NSError *error;
+    CBLManager* dbManager = [CBLManager sharedInstance];
+    CBLDatabase* database = [dbManager existingDatabaseNamed:databaseLocal error: &error];
+    if (!database) {
+        NSString* cannedDbPath = [[NSBundle mainBundle] pathForResource: withPreBuildDatabase
+                                                                 ofType: @"cblite"];
+        BOOL ok = [dbManager replaceDatabaseNamed: databaseLocal
+                                 withDatabaseFile: cannedDbPath
+                                  withAttachments: nil
+                                            error: &error];
+        if (!ok) {
+            NSLog(@"database '%@' copy fail", databaseLocal);
+            callback(@[@"copy fail"]);
+        } else {
+            database = [dbManager existingDatabaseNamed: @"catalog" error: &error];
+            NSLog(@"database '%@' copy succ", databaseLocal);
+            callback(@[[NSNull null]]);
+        }
+    } else {
+        NSLog(@"database '%@' already exist", databaseLocal);
+        callback(@[@"database already exist"]);
+    }
 }
 
 @end
